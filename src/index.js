d8256755de8b46e92f6ba9664b91172d9bbedaff
Filip <filipelblag@gmail.com>
2023-05-17 13:18:21 +0200

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Redux Setup
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = configureStore({reducer: rootReducer});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
